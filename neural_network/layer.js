49afe145c28d53b94fd2d1db53eeb73731fae663
Hayley Owens <hayleyjeanowens@gmail.com>
2021-09-28 17:17:10 -0500

    Added dictionary with activation function formulas

--- neural_network/layer.js
+++ neural_network/layer.js
@@ -2,10 +2,11 @@
 
 class Layer 
 {
+    activation_functions = {'sigmoid':'(1)/(1+e^(-x))', 'tanh':'((2)/(1+e^(-2x)))-1'};
     constructor(nodes, activation_function, layer_type) 
     {
       self.nodes = nodes;
-      //Have an enum or object of some sort with activation function type pointing to a particular activation function formula
+      self.activation_function = activation_function//Have an enum or object of some sort with activation function type pointing to a particular activation function formula
     }
 
 }
