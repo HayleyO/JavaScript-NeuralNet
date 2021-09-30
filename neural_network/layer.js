fdb7b6bd1cb72d5497ee3e3c6cedc85eae9153e1
Hayley Owens <hayleyjeanowens@gmail.com>
2021-09-30 09:55:20 -0500

    Handling if layer set something it doens't have

--- neural_network/layer.js
+++ neural_network/layer.js
@@ -3,13 +3,24 @@
 class Layer 
 {
     activation_functions = {'sigmoid':'(1)/(1+e^(-x))', 'tanh':'((2)/(1+e^(-2x)))-1'};
-    layer_type = {'sequential': 'insert_object with particular ways to do it', 'convolutional': 'yeah we havent gotten there yet'}
+    layer_types = {'sequential': 'insert_object with particular ways to do it', 'convolutional': 'yeah we havent gotten there yet'}
     constructor(nodes, activation_function = 'input', layer_type) 
     {
-      //Layers will use activation functions of next layer to feed forward and a final activation function to 
+      //Layers will use activation functions of next layer to feed forward 
       self.nodes = nodes;
-      self.activation_function = activation_function//Have an enum or object of some sort with activation function type pointing to a particular activation function formula
-      self.layer_type = layer_type
+      if (!(activation_function in activation_functions))
+      {
+        throw 'Unknown activation function, try: ' + Object.keys(activation_functions);
+      }
+      else if(!(layer_type in layer_types))
+      {
+        throw 'Unknown layer type, try: ' + Object.keys(layer_type);
+      }
+      else
+      {
+        self.activation_function = activation_function//Have an enum or object of some sort with activation function type pointing to a particular activation function formula
+        self.layer_type = layer_type
+      }  
     }
 
 }
