43ec15cef4b776776126321df206b9bfc4c4313a
Hayley Owens <hayleyjeanowens@gmail.com>
2021-09-30 17:43:21 -0500

    More baseline

--- neural_network/layers/layer.js
+++ neural_network/layers/layer.js
@@ -3,24 +3,27 @@
 class Layer 
 {
     activation_functions = {'sigmoid':'(1)/(1+e^(-x))', 'tanh':'((2)/(1+e^(-2x)))-1'};
-    layer_types = {'sequential': 'insert_object with particular ways to do it', 'convolutional': 'yeah we havent gotten there yet'}
-    constructor(nodes, activation_function = 'input', layer_type) 
+    constructor(nodes, activation_function = 'input') 
     {
-      //Layers will use activation functions of next layer to feed forward 
       self.nodes = nodes;
       if (!(activation_function in activation_functions))
       {
         throw 'Unknown activation function, try: ' + Object.keys(activation_functions);
       }
-      else if(!(layer_type in layer_types))
-      {
-        throw 'Unknown layer type, try: ' + Object.keys(layer_type);
-      }
       else
       {
         self.activation_function = activation_function//Have an enum or object of some sort with activation function type pointing to a particular activation function formula
-        self.layer_type = layer_type
       }  
     }
 
+    forwardpass()//param: next_layer
+    {
+      // (weights X inputs)+biases
+      // evaluate from next layer's activation function 
+    }
+
+    backwardpass()
+    {
+
+    }
 }
--- neural_network/neural_network.js
+++ neural_network/neural_network.js
@@ -2,24 +2,26 @@
 
 class Neural_Network 
 {
-    //variable for matrixes of weights and biases
     constructor(input_dim, output_dim, optimizer_type, minibatches, epochs, learning_rate) 
     {
-      this.input_dim = input_dim  //How will we handle images? I'm thinking of just flattening the matrix so a 2x2 becomes a 4x1 for ease of use
+      this.input_dim = input_dim  
       this.output_dim = output_dim
 
       this.optimizer = new Optimizer(optimizer_type, minibatches, epochs, learning_rate)
     }
 
     static layers = [new Layer(this.input_dim, 'input', 'default_ff')]
+    static weights = []
+    static biases = []
     
     add_layer(nodes, activation_function, layer_type)
     {
       layers.splice((layers.length), 0, new Layer(nodes, activation_function, layer_type));
     }
-    
+
     generate_weights_and_biases()
     {
+      //generate based on dimensions of input (how do we muliple dimensional input)
       if(layers.length > 1)
       {
         for (let layer = 0; layer < layers.length -1; layer++)
@@ -46,12 +48,15 @@ class Neural_Network
         throw 'Not enough layers to generate weights and biases.'
       }
     }
-    
+
     feedforward()
     {
-      //for each layer
-      // (weights X inputs)+biases
-      // evaluate from current layer's activation function 
+      //For each layer, layer.feedforward, pass in current weight
+    }
+
+    train()
+    {
+      //optimizer.sgd
     }
 }
 
