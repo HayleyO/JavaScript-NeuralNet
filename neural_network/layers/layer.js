84d43cfbb9ea645ccf9e896dddf3f44d5e843197
Hayley Owens <hayleyjeanowens@gmail.com>
2021-10-01 20:04:41 -0500

    Lots of structuring

--- neural_network/layers/layer.js
+++ neural_network/layers/layer.js
@@ -1,8 +1,8 @@
-
+import matrix from "math.js";
 
 class Layer 
 {
-    activation_functions = {'sigmoid':'(1)/(1+e^(-x))', 'tanh':'((2)/(1+e^(-2x)))-1'};
+    activation_functions = {'input': '1', 'sigmoid':'(1)/(1+e^(-x))', 'tanh':'((2)/(1+e^(-2x)))-1'};
     constructor(nodes, activation_function = 'input') 
     {
       self.nodes = nodes;
@@ -14,6 +14,14 @@ class Layer
       {
         self.activation_function = activation_function//Have an enum or object of some sort with activation function type pointing to a particular activation function formula
       }  
+    }
+
+    generate_layer_weights_and_biases(next_layer)
+    {
+      self.weights = math.ones(next_layer.nodes, self.nodes)
+      self.biases = math.ones(next_layer.nodes, 1)
+      math.map(self.weights, x => x*Math.random())
+      math.map(self.biases, x => x*Math.random())
     }
 
     forwardpass()//param: next_layer
